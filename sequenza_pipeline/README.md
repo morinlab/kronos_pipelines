c6f910cf2419fc2abf1182ff861d7765643ec7ae
Bruno Grande <bgrande@sfu.ca>
2016-01-14 12:11:10 -0800

    updated sequenza pipeline
    - added launch.sh
    - updated setup for genesis

--- sequenza_pipeline/README.md
+++ sequenza_pipeline/README.md
@@ -1,3 +1,5 @@
 # Sequenza Pipeline
 
-This pipeline's `setup.tsv` hasn't been completed for Genesis. 
+## Notes
+
+1. A GC content file is needed for every reference genome that exists. By default, we're using 50-base windows. As you generate these, please include them in the Annotation/GC_Content of your reference build in `igenomes`. Include the window size in the file name, _e.g._ `GRCh37-lite.gc50Base.txt.gz`. 
---
+++ sequenza_pipeline/launch.sh
@@ -0,0 +1,15 @@
+NAME="sequenza_pipeline"
+VERSION="3.1"
+/extscratch/morinlab/software/anaconda/2.3.0/envs/kronos-2.0.4/bin/kronos \
+    run \
+        --working_dir . \
+        --pipeline_name "$NAME" \
+        --config_file "/extscratch/morinlab/software/pipelines_morinlab/$VERSION/$NAME/tasks.yaml" \
+        --setup_file "/extscratch/morinlab/software/pipelines_morinlab/$VERSION/$NAME/setup.GRCh37-lite.genesis.tsv" \
+        --input_samples "samples.tsv" \
+        --job_scheduler "sge" \
+        --components_dir "/extscratch/morinlab/software/pipeline_components_shahlab/cycle008.3" \
+        --python_installation "/extscratch/morinlab/software/anaconda/2.3.0/envs/kronos-2.0.4/bin/python" \
+        --num_pipelines "10" \
+        --num_jobs "200" \
+        --qsub_options ' -pe ncpus {num_cpus} -l mem_free={mem} -l mem_token={mem} -l h_vmem={mem} -w n'
