35f1ddc0b90aa08277d93e7406b5e58890d7fa0e
Bruno Grande <bgrande@sfu.ca>
2016-01-26 11:41:14 -0800

    Fix components version in launch.sh

--- strelka_pipeline/launch.sh
+++ strelka_pipeline/launch.sh
@@ -1,5 +1,5 @@
 NAME="sequenza_pipeline"
-VERSION="3.4"
+VERSION="4.6"
 /extscratch/morinlab/software/anaconda/2.3.0/envs/kronos-2.0.4/bin/kronos \
     run \
         --working_dir . \
@@ -8,7 +8,7 @@ VERSION="3.4"
         --setup_file "/extscratch/morinlab/software/pipelines_morinlab/$VERSION/$NAME/setup.GRCh37-lite.genesis.tsv" \
         --input_samples "samples.tsv" \
         --job_scheduler "sge" \
-        --components_dir "/extscratch/morinlab/software/pipeline_components_morinlab/6.1" \
+        --components_dir "/extscratch/morinlab/software/pipeline_components_morinlab/6.2" \
         --python_installation "/extscratch/morinlab/software/anaconda/2.3.0/envs/kronos-2.0.4/bin/python" \
         --num_pipelines "10" \
         --num_jobs "200" \
