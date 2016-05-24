d24d8deca951243ea7b219c87d8285662a63613c
Selin Jessa <selinjessa@gmail.com>
2016-05-24 10:34:02 -0700

    Update kronos version

--- realignment_pipeline/launch_genesis.sh
+++ realignment_pipeline/launch_genesis.sh
@@ -1,6 +1,6 @@
 VERSION="6.1"
 COMPONENT_VERSION="8.0"
-/extscratch/morinlab/software/anaconda/2.3.0/envs/kronos-2.0.4/bin/kronos run \
+/extscratch/morinlab/software/anaconda/2.3.0/envs/kronos-2.1.0_beta/bin/kronos run \
 -y /extscratch/morinlab/software/pipelines_morinlab/$VERSION/realignment_pipeline/realignment_pipeline_linear.yaml \
 -w ./ \
 -i samples.tsv \
@@ -8,7 +8,7 @@ COMPONENT_VERSION="8.0"
 -c /extscratch/morinlab/software/pipeline_components_morinlab/$COMPONENT_VERSION/ \
 -b sge \
 --num_pipelines 10 \
--j 400 \
+-j 20 \
 -p /extscratch/morinlab/software/anaconda/2.3.0/envs/kronos-2.0.4/bin/python2.7 \
 --qsub_options ' -pe ncpus {num_cpus} -l mem_free={mem} -l mem_token={mem} -l h_vmem={mem} -w n -S /bin/sh' \
 --no_prefix
